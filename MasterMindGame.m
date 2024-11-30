e808ce010e1e7a43074d95ac59fea61dadd5b8cb
tariktufan <84205502+tariktufan@users.noreply.github.com>
2024-11-30 22:16:40 +0300

    Update MasterMindGame.m

--- MasterMindGame.m
+++ MasterMindGame.m
@@ -1,15 +1,15 @@
 rkm=4;arti=0;
-Aralik=TumFarkliRakam(rkm); %Rakamlari farkli basamakli sayilar
-DarAralik=Aralik; %Kullanıcı cevabına göre daraltılmış aralık
-hata=0; %kullanici hatasiz girmis kabulu ile başlar
-Thmn=[]; % Tahmin edilen sayi ve + - değerlerini barındırır;
+Aralik=TumFarkliRakam(rkm); %Rakamlari farkli basamakli sayilar %ENG:Numbers with unique digits
+DarAralik=Aralik; %Kullanıcı cevabına göre daraltılmış aralık %ENG:Narrowed range based on user response
+hata=0; %kullanici hatasiz girmis kabulu ile başlar %ENG: assuming the user has entered without errors.
+Thmn=[]; % Tahmin edilen sayi ve + - değerlerini barındırır; %ENG:Contains predicted numbers and + - values
 while arti<rkm
-    ThmnY=DarAralik(randi(size(DarAralik,1)),:); % Tahmini belirler
-    TahminYazdir(ThmnY); % Tahmini Ekrana yazdırır
+    ThmnY=DarAralik(randi(size(DarAralik,1)),:); % Tahmini belirler %ENG: estimating the number
+    TahminYazdir(ThmnY); % Tahmini Ekrana yazdırır %ENG:fprintfs the estimate
     DD=1;
     while DD 
-        arti=input('+:');   % Kullanıcı artı değerini girer
-        eksi=input('-:');   % Kullanıcı eksi değerini girer
+        arti=input('+:');   % Kullanıcı artı değerini girer %ENG:positives
+        eksi=input('-:');   % Kullanıcı eksi değerini girer %ENG:negatives
         if arti+eksi<=rkm
             DD=0;
         else
@@ -19,6 +19,6 @@ while arti<rkm
     if arti<4 && size(DarAralik,1)==1
         hata=hata+1;
     end
-    Thmn=[Thmn;ThmnY,arti,eksi]; % Kaydedilir.
-    [DarAralik,hata]=Ara(Thmn,Aralik,hata); %Kriter ve hataya uygun Aralik Bulunur.
+    Thmn=[Thmn;ThmnY,arti,eksi]; % Kaydedilir. %Save the estimate and its score
+    [DarAralik,hata]=Ara(Thmn,Aralik,hata); %Kriter ve hataya uygun Aralik Bulunur. %Redefines the according numbers
 end
